01f59b0d63abd2115863f101d63c6218886bb106
K M Rejowan Ahmmed <rejowan@righttracksit.com>
2025-09-17 12:43:08 +0600

    splash, logo

--- app/src/main/java/com/rejowan/multiappuninstaller/HomeActivity.kt
+++ app/src/main/java/com/rejowan/multiappuninstaller/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.rejowan.multiappuninstaller.feature.module.home.HomeScreen
 import com.rejowan.multiappuninstaller.ui.theme.MAUTheme
 
@@ -11,6 +12,7 @@ class HomeActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             MAUTheme {
